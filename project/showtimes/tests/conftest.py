75b128b902e0993160c23c57326a431ebbf24bb0
saradonin <m.jemieljanczuk@gmail.com>
2023-08-26 21:40:01 +0200

    some imports fixed

--- project/showtimes/tests/conftest.py
+++ project/showtimes/tests/conftest.py
@@ -5,9 +5,9 @@ import pytest
 from faker import Faker
 from rest_framework.test import APIClient
 
-from movielist import Person
-from movielist import create_fake_movie
-from showtimes.tests import create_fake_cinema
+from movielist.models import Person
+from movielist.tests.utils import create_fake_movie
+from showtimes.tests.utils import create_fake_cinema
 
 sys.path.append(os.path.dirname(__file__))
 faker = Faker("pl_PL")
